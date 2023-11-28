ffbbb64c9d26077b8190541e725cf947bd2dc62d
kaualira <130668344+kaualira@users.noreply.github.com>
2023-11-28 09:37:21 -0300

    comit do viny

---
+++ calculadoradejuros.py
@@ -0,0 +1,10 @@
+# Progama que diz bom dia para o usuario
+
+# Apresentação
+print('\n\t\t\t -- dia bom dia ao Usuariom-- \n')
+
+# Entreda
+nome = input('Informe o seu nome: ')
+
+#Saída
+print('Bom dia ' + nome +'.')
